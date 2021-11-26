605901a746b0a351f55b5893741d39097f3d8e7b
dholleman <d.holleman@student.utwente.nl>
2021-11-26 14:16:30 +0100

    Initial Commit

--- easyFiles/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name='easyFiles',
-	packages=find_packages(include=['easyFiles']),
-    version='0.1.0',
-    description='Easy File finding.',
-    author='Daan Holleman',
-	install_requires=['numpy', 'glob', 'os'],
-)
